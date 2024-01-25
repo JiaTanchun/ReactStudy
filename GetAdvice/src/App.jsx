dbde0bc586a04e0138cfc743b2f5265bbfd8a5d1
Tanchun <123371260+JiaTanchun@users.noreply.github.com>
2024-01-25 12:21:29 -0500

    getElementById_createRoot

--- GetAdvice/src/App.jsx
+++ GetAdvice/src/App.jsx
@@ -1,30 +1,30 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 
-export default function APP(){
+export default function APP() {
 
-  const [advice,setAdvice]=useState("")
-  const [count, setCount]=useState(0)
+  const [advice, setAdvice] = useState("")
+  const [count, setCount] = useState(0)
 
-  async function getAdvice(){
-    const res=await fetch("https://api.adviceslip.com/advice")
-    const data=await res.json();
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json();
     setAdvice(data.slip.advice);
-    setCount((c)=>c+1)
+    setCount((c) => c + 1)
   }
-  useEffect(function(){
+  useEffect(function () {
     getAdvice()
-  },[]
+  }, []
 
   )
 
   return <div>
     <h1>{advice}</h1>
     <button onClick={getAdvice}>Get Advice</button>
-    <Message count={count}/>
+    <Message count={count} />
   </div>
 }
 
-function Message(props){
+function Message(props) {
   return (
     <p>
       You have read <strong>{props.count}</strong> pieces of advice
